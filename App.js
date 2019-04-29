195b34988e425e90081d1fa245ac4dd669e02c18
Stefan Gustavsson <stefgust@gmail.com>
2019-04-29 06:15:53 +0200

    just testing

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if(req.url === '/')
+    {
+        res.write('Hello World');
+        res.end();
+    }
+    if(req.url === '/api/courses')
+    {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+
+});
+
+server.listen(3000);
+console.log('Listening on port 3000....');
+
+/*
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
 //Register listnerer
-emitter.on('messageLogged', function(eventArgs){
+emitter.on('messageLogged', (eventArgs) => {
     console.log('Listener called', eventArgs)
 });
 
-emitter.emit('messageLogged', { id:1, url: 'https://hej.se'});
+const Logger = require('./Logger');
+const logger = new Logger();
+logger.on('messageLogged', (eventArgs) => {
+    console.log('Listener called', eventArgs)
+});
+
+logger.log('message');
+*/
---
+++ Logger.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+var url = 'https://mylogger.io.log';
+
+class Logger extends EventEmitter
+{
+    log(message)
+    {
+        //log message
+        console.log(message);
+        this.emit('messageLogged', {id: 1, url: 'http://'});
+    }
+}
+
+//module.exports.log = log;
+module.exports = Logger;
